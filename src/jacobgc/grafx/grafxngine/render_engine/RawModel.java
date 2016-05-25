d5ea107196ef714a1b3f2383f2ce35392972949d
JacobGC <jacob.gettmanncoons@gmail.com>
2016-05-25 00:35:08 -0400

    This is my last commit for today, and here is what I have accomplished: a basic vao/vbo rendering engine.

---
+++ src/jacobgc/grafx/grafxngine/render_engine/RawModel.java
@@ -0,0 +1,7 @@
+package jacobgc.grafx.grafxngine.render_engine;
+
+/**
+ * Created by JacobGC on 5/24/2016.
+ */
+public class RawModel {
+}
---
+++ src/jacobgc/grafx/grafxngine/render_engine/Renderer.java
@@ -0,0 +1,7 @@
+package jacobgc.grafx.grafxngine.render_engine;
+
+/**
+ * Created by JacobGC on 5/25/2016.
+ */
+public class Renderer {
+}
